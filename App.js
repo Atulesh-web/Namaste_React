c019903e4ef708d7ccb68467fbfcd911fb1d9919
Atulesh-web <57455225+Atulesh-web@users.noreply.github.com>
2023-12-23 17:05:48 +0530

    First video

---
+++ App.js
@@ -0,0 +1,9 @@
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading1 = React.createElement("h1", {className : "heading"}, "Welcome to React JS");
+const heading2 = React.createElement("h2",{className : "heading2"}, "Hi there");
+const child1 = React.createElement("div", {id:"child"}, [heading1, heading2,heading1]);
+const child2 = React.createElement("div", {id:"child2"}, [heading1, heading2,heading1]);
+const parent = React.createElement("div", {id:"parent"}, [child1,child2]);
+
+root.render(parent); 
