b60d0536dd0d6134f318908035f47e85e6884765
Atulesh-web <atuleshsharma5@gmail.com>
2023-12-25 13:25:18 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
-import React  from "react";
-import ReactDOM  from "react-dom/client";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const heading1 = React.createElement("h1", {className : "heading"}, "Welcome to React JS");
-const heading2 = React.createElement("h2",{className : "heading2"}, "Hi there");
-const child1 = React.createElement("div", {id:"child"}, [heading1, heading2,heading1]);
-const child2 = React.createElement("div", {id:"child2"}, [heading1, heading2,heading1]);
-const parent = React.createElement("div", {id:"parent"}, [child1,child2]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-root.render(parent);
+// USING CORE REACT
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const heading1 = React.createElement("h1", {className : "heading"}, "Welcome to React JS");
+// const heading2 = React.createElement("h2",{className : "heading2"}, "Hi there");
+// const child1 = React.createElement("div", {id:"child"}, [heading1, heading2,heading1]);
+// const child2 = React.createElement("div", {id:"child2"}, [heading1, heading2,heading1]);
+// const parent = React.createElement("div", {id:"parent"}, [child1,child2]);
+// root.render(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+///CORE REACT
+// const heading = React.createElement("h1",{className:"heading2"},"I am so HAPPY 😁 for starting React.js")
+
+///JSX - is not HTML but HTML LIKE SYNTAX
+///BABLE converts this JSX code into React.createElement
+const JsxHeading = (
+  <div>
+    <div>
+      <h1 className="heading">Hi there, this is written using JSX 😁</h1>
+    </div>
+    <div>
+      <h1 className="heading2">Trying multiple lines jsx</h1>
+    </div>
+  </div>
+);
+
+// React Functional Component
+const HeadingComponent = ()=>{
+
+  return <div>
+    {JsxHeading}
+    <h1>This is the my first functional React Component.</h1>
+    </div>;
+}
+console.log(HeadingComponent)
+root.render(<HeadingComponent/>);
+ 
